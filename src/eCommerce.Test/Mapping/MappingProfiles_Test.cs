5c4add5f02d58a863d7f380d0af38fbc185a3c78
3mrotaha <amr.taha1261@gmail.com>
2026-06-18 07:17:25 +0300

    added unit tests for the services, repositories, mappers, infrastructure .. etc - fix

--- src/eCommerce.Test/Mapping/MappingProfiles_Test.cs
+++ src/eCommerce.Test/Mapping/MappingProfiles_Test.cs
@@ -69,7 +69,7 @@ public class MappingProfiles_Test
 
         var dto = _mapper.Map<ProductResponseDto>(product);
 
-        dto.CategoryName.Should().Be("Electronicsgg");
+        dto.CategoryName.Should().Be("Electronics");
     }
 
     [Fact]
